695fb5a941595aa430dd694705dd1c87c4c31ada
Anton Svitenkov <svitenkov@gmail.com>
2018-12-20 20:00:21 +0300

    update test application

--- samples/UefiApplication/helloapp.c
+++ samples/UefiApplication/helloapp.c
@@ -75,86 +75,11 @@ UefiMain (
     IN EFI_SYSTEM_TABLE* SystemTable
     )
 {
-    EFI_STATUS efiStatus;
-    SHELL_FILE_HANDLE fileHandle;
-    UINT8 buffer[4];
-    UINTN readSize;
-    EFI_SAMPLE_DRIVER_PROTOCOL* sampleProtocol;
 
-    // 
-    // Print stuff out 
-    // 
-    fileHandle = NULL;
+
     Print(L"Hello World! My handle is %lx and System Table is at %p\n",
           ImageHandle, SystemTable);
 
-    //
-    // Initialize the shell library
-    //
-    efiStatus = ShellInitialize();
-    if (EFI_ERROR(efiStatus))
-    {
-        Print(L"Failed to initialize shell: %lx\n", efiStatus);
-        goto Exit;
-    }
-
-    //
-    // Open ourselves
-    //
-    efiStatus = ShellOpenFileByName(L"fs1:\\UefiApplication.efi",
-                                    &fileHandle,
-                                    EFI_FILE_MODE_READ,
-                                    0);
-    if (EFI_ERROR(efiStatus))
-    {
-        Print(L"Failed to open ourselves: %lx\n", efiStatus);
-        fileHandle = NULL;
-        goto Exit;
-    }
-
-    //
-    // Read 4 bytes at the top (MZ header)
-    //
-    readSize = sizeof(buffer);
-    efiStatus = ShellReadFile(fileHandle, &readSize, &buffer);
-    if (EFI_ERROR(efiStatus))
-    {
-        Print(L"Failed to read ourselves: %lx\n", efiStatus);
-        goto Exit;
-    }
-
-    //
-    // Print it
-    //
-    Print(L"Data: %lx\n", *(UINT32*)buffer);
-
-    // 
-    // Check if the sample driver is loaded 
-    // 
-    efiStatus = gBS->LocateProtocol(&gEfiSampleDriverProtocolGuid, NULL, &sampleProtocol);
-    if (EFI_ERROR(efiStatus))
-    {
-        Print(L"Failed to locate our driver: %lx\n", efiStatus);
-        goto Exit;
-    }
-
-    // 
-    // Print the value and exit 
-    // 
-    Print(L"Sample driver is loaded: %lx\n", sampleProtocol->SampleValue);
-
-Exit:
-    //
-    // Close our file handle
-    //
-    if (fileHandle != NULL)
-    {
-        ShellCloseFile(&fileHandle);
-    }
-
-    //
-    // Sample complete!
-    //
-    return efiStatus;
+    return EFI_SUCCESS;
 }
 
